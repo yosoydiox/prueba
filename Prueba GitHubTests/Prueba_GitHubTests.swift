c43d00af4dc279b2b878b866d925bec392b5d209
Patricio Gonzalez Sanchez <pehuenche69@gmail.com>
2016-05-09 11:50:19 -0300

    Initial Commit

---
+++ Prueba GitHubTests/Prueba_GitHubTests.swift
@@ -0,0 +1,36 @@
+//
+//  Prueba_GitHubTests.swift
+//  Prueba GitHubTests
+//
+//  Created by Patricio Gonzalez Sanchez on 09-05-16.
+//  Copyright © 2016 Patricio Gonzalez Sanchez. All rights reserved.
+//
+
+import XCTest
+@testable import Prueba_GitHub
+
+class Prueba_GitHubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
